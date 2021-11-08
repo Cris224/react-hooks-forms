9e03bf701d5893b300437f5ff1e267bdc04f88d1
Cristofer <cristoferlorenzana@gmail.com>
2021-11-08 09:10:34 -0600

    commit tree af70f071f8f3acf9b8c0b0e6dc1393ad801bc969

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,18 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFisrtNameChange(e){
+    setFirstName(e.target.value)
+  }
+
+  function handleLastNameChange(e){
+    setLastName(e.target.value)
+  }
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={handleFisrtNameChange} />
+      <input type="text" value={lastName} onChange={handleLastNameChange}/>
       <button type="submit">Submit</button>
     </form>
   );
